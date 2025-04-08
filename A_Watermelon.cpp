57c59d59ccd96895b33efa892697506e8bdd862c
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-04-08 08:21:57 +0600

    Add files via upload

---
+++ A_Watermelon.cpp
@@ -0,0 +1,15 @@
+//08.04.2025
+//rafy
+//basic 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
