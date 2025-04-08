527f9ca302d435b3a9c2b1420310e781e1ad5a0b
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-04-09 01:50:48 +0600

    Add files via upload

---
+++ A_Bit.cpp
@@ -0,0 +1,20 @@
+//09.04.2025
+//rafy
+//increment and decrement operator
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,d=0;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s[1]=='+'){
+           d++;
+        }
+        else{
+            d--;
+        }
+        }
+    cout<<d<<endl;
+}
