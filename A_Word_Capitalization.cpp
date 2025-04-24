538da968071929ed0a2663c0be00c685a60261a4
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-04-24 10:06:16 +0600

    Add files via upload

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,13 @@
+//24.04.2025
+//rafy
+//frist latter capital 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    if(!s.empty()){
+        s[0]=toupper(s[0]);
+    }
+    cout<<s<<endl;
+}
