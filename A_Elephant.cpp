78cfa9c7624b65249c1706a3ba4619469ebdb7d4
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-04-25 23:29:25 +0600

    Add files via upload

---
+++ A_Elephant.cpp
@@ -0,0 +1,14 @@
+//25.4.2025
+//rafy
+//logic implement
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int e=n/5;
+    if(n%5!=0){
+        e++;
+    }
+    cout<<e<<endl;
+}
