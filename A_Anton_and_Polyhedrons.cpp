00885f8d0e132e8911d170d3d820eade18a14ded
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-05-21 23:17:08 +0600

    Add files via upload

---
+++ A_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,18 @@
+//21.5.25
+//rafy
+//string
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    int sum=0;
+    map<string,int> ww={
+        {"Icosahedron",20},{"Cube",6},{"Tetrahedron",4},{"Dodecahedron",12},{"Octahedron",8}
+    };
+    while(n--){
+        string s;cin>>s;
+        sum+=ww[s];
+    }
+    cout<<sum<<endl;
+
+}
