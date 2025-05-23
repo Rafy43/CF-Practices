67b3cfad0698843e5ffb65d12d9c7de5bb2ae4e1
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-05-23 08:25:00 +0600

    Add files via upload

---
+++ A_Gravity_Flip.cpp
@@ -0,0 +1,16 @@
+//23.5.25
+//rafy
+//sroting
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
