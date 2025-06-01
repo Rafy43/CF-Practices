e15f8551a1f944b4b95a8f89485f1d711878fb1c
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-06-01 22:47:51 +0600

    Add files via upload

---
+++ A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,19 @@
+//1.6.25
+//rafy
+//basic math
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        long long n,a;
+        cin>>n;
+        a=(n-1)/2;
+        if(3<=n){
+            cout<<a<<endl;
+        }
+        else{
+            cout<<0<<endl;
+        }
+    }
+}
