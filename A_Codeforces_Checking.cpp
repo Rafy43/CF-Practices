904d0e4d4559af6b4e0384b5a08373f49a9bb431
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-06-27 22:58:43 +0600

    Add files via upload

---
+++ A_Codeforces_Checking.cpp
@@ -0,0 +1,17 @@
+//27.06.2025
+//rafy
+//string
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        char c;cin>>c;
+        if(c=='c'||c=='o'||c=='d'||c=='e'||c=='f'||c=='o'||c=='r'||c=='c'||c=='e'||c=='s'){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}  
