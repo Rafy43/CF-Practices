f9a4c01e6df71a5f80af741e5b8a11dc5c3be681
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-07-08 10:11:25 +0600

    Add files via upload

---
+++ A_Odd_One_Out.cpp
@@ -0,0 +1,23 @@
+//8.7.2025
+//rafy
+//normal implement
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int arr[3];
+        for(int i=0;i<3;i++){
+            cin>>arr[i];
+        }
+        if(arr[0]==arr[1]){
+            cout<<arr[2]<<endl;
+        }
+        else if(arr[1]==arr[2]){
+            cout<<arr[0]<<endl;
+        }
+        else{
+            cout<<arr[1]<<endl;
+        }
+    }
+}
