52000dda20237e2a2882556081e80d876fc67f8f
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-07-12 10:42:03 +0600

    Add files via upload

---
+++ A_Plus_or_Minus.cpp
@@ -0,0 +1,16 @@
+//12.7.25
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c){
+            cout<<"+"<<endl;
+        }
+        else{
+            cout<<"-"<<endl;
+        }
+    }
+}
